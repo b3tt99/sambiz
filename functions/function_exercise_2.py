6c4d1551c0f3ef4e64c7a4bc38747e1b1c95cab4
eMobilis <b3tt99@gmail.com>
2023-02-13 09:05:16 +0300

    first commit

---
+++ functions/function_exercise_2.py
@@ -0,0 +1,9 @@
+#write a function that will take 2 numbers
+#multiply the two numbers and print the result
+def calculate_product() :
+    first = int(input("Enter first number"))
+    second = int(input("Enter second number"))
+    product = first * second
+    print(f"The product is {product}")
+calculate_product()
+
